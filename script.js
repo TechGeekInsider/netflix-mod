0631acd1ff51b6881fa74bb3f17d20ee0c4443be
Nick <lesseosnick@gmail.com>
2018-02-11 17:08:03 -0800

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 
-//  $("body").append(<h1>'Test'</h1>);
-$( document ).ajaxSuccess(function(){
-    $(".slider-item").hover(function(){
-        console.log("Pandas are evil!");
-    })
 
-});
+
+var observer = new MutationObserver(
+    function(){
+    $(".slider-item").mouseover(function(){
+        var show = $(this).find(".video-preload-title-label")[0].textContent;
+        console.dir(show);
+        var score = "Not Found";
+        if(show == "Altered Carbon"){
+            score = "62.9%";
+        }
+        if(show == "Black Mirror"){
+            score = "97.3%";
+        }
+        if(show == "Dirty Money"){
+            score = "72.6%";
+        }
+        if(show == "Pirates of the Caribbean: Dead Men Tell No Tales"){
+            score = "52.3%";
+        }
+        if(show == "Dave Chappelle: Equanimity & The Bird Revelation"){
+            score = "98.2%";
+        }
+        if(show == "The Office (U.S.)"){
+            score = "85.3%";
+        }
+        if(show == "A Futile and Stupid Gesture"){
+            score = "42.1%";
+        }
+        $(this).find(".bob-overlay").html("<div><h3>Rotten Tomatoe Score: " + score + "</h3></div>").css("position: absolute")
+    })
+    }
+  );
+observer.observe(document.getElementsByClassName('lolomo')[0], {childList:true});
