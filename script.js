4f88864b9b9453d44c89cce5bbc9725ee0fbc19f
Nick Lesseos <lesseosnick@gmail.com>
2023-09-21 16:01:04 -0700

    Added: make call to go serverless api

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ var observer = new MutationObserver(function() {
       // Check if the event listener has already been attached
       if (!sliderItem.dataset.processed) {
           sliderItem.dataset.processed = 'true'; // Mark the element as processed
-          sliderItem.addEventListener('mouseenter', function() {
+          sliderItem.addEventListener('mouseenter', async function() {
               var show = sliderItem.querySelector('.title-card-container').firstChild.firstChild.firstChild.innerText;
               var score = 'Not Found';
+
+              try {
+                  var response = await fetch("https://movie-data-ag9v36bd8-technosapien.vercel.app/api/search?q=" + encodeURIComponent(show));
+                  var data = await response.json(); // Await the reading of the response body
+                  console.log(data);
+                  // Update the score or any other logic based on the data
+                  score = data.imdb_rating || 'Not Found';
+              } catch (error) {
+                  console.error("Error fetching data:", error);
+              }
 
               console.log(show);
 
@@ -16,13 +26,13 @@ var observer = new MutationObserver(function() {
                   console.log("Hitzone", hitzone);
 
                   // Check if the score element already exists in the hitzone
-                  if (!hitzone.querySelector('.metacritic-score')) {
+                  if (!hitzone.querySelector('.mediainsights-score')) {
                       var scoreElement = document.createElement('div');
-                      scoreElement.className = 'metacritic-score'; // Add a class for identification
-                      scoreElement.innerHTML = '<h3>MetaCrtic Score: ' + score + '</h3>';
+                      scoreElement.className = 'mediainsights-score'; // Add a class for identification
+                      scoreElement.innerHTML = '<h3>IMDB Score: ' + score + '</h3>';
                       hitzone.appendChild(scoreElement);
                   }
-              }, 1000);
+              }, 400);
           });
       }
   });
