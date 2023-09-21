479e3e0b84e82672469cbbf314d3945bec252a68
Nick Lesseos <lesseosnick@gmail.com>
2023-09-21 15:06:57 -0700

    Updated: netflix injection

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 var observer = new MutationObserver(function() {
-  $(".slider-item").mouseover(function() {
-    var show = $(this).find(".title-card-container")[0].firstChild.firstChild
-      .firstChild.innerText;
-    var score = "Not Found";
-    if (show == "Altered Carbon") {
-      score = "62.9%";
-    }
-    if (show == "Black Mirror") {
-      score = "97.3%";
-    }
-    if (show == "Dirty Money") {
-      score = "72.6%";
-    }
-    if (show == "Pirates of the Caribbean: Dead Men Tell No Tales") {
-      score = "52.3%";
-    }
-    if (show == "Dave Chappelle: Equanimity & The Bird Revelation") {
-      score = "98.2%";
-    }
-    if (show == "The Office (U.S.)") {
-      score = "85.3%";
-    }
-    if (show == "A Futile and Stupid Gesture") {
-      score = "42.1%";
-    }
-    $(this)
-      .find(".bob-play-hitzone")
-      .html("<div><h3>Rotten Tomatoe Score: " + score + "</h3></div>")
-      .css("position: absolute");
+  document.querySelectorAll('.slider-item').forEach(function(sliderItem) {
+      // Check if the event listener has already been attached
+      if (!sliderItem.dataset.processed) {
+          sliderItem.dataset.processed = 'true'; // Mark the element as processed
+          sliderItem.addEventListener('mouseenter', function() {
+              var show = sliderItem.querySelector('.title-card-container').firstChild.firstChild.firstChild.innerText;
+              var score = 'Not Found';
+
+              console.log(show);
+
+              setTimeout(function() {
+                  var hitzone = document.querySelector('.previewModal--wrapper');
+                  hitzone = hitzone.firstChild.firstChild.lastChild.firstChild;
+                  
+                  console.log("Hitzone", hitzone);
+
+                  // Check if the score element already exists in the hitzone
+                  if (!hitzone.querySelector('.metacritic-score')) {
+                      var scoreElement = document.createElement('div');
+                      scoreElement.className = 'metacritic-score'; // Add a class for identification
+                      scoreElement.innerHTML = '<h3>MetaCrtic Score: ' + score + '</h3>';
+                      hitzone.appendChild(scoreElement);
+                  }
+              }, 1000);
+          });
+      }
   });
 });
-observer.observe(document.getElementsByClassName("lolomo")[0], {
-  childList: true
+
+observer.observe(document.querySelector('.lolomo'), {
+  childList: true,
+  subtree: true // Observe changes to descendants
 });
